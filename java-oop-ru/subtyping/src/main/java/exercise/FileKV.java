dce5b7669abd43dad080fd6463d934fd1a96ca3d
@hexlet/cli <support@hexlet.io>
2022-12-30 15:54:59 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -1,87 +1,44 @@
 package exercise;
 
+import java.util.HashMap;
 import java.util.Map;
 
 // BEGIN
 public class FileKV implements KeyValueStorage {
-       /* Конструктор класса принимает два аргумента: Путь до файла, в котором будут храниться данные (строка)
-        Словарь Map, который будет начальным значением базы данных. Ключи и значения в словаре также представлены строками.*/
+
         private final String path;
-        private final Map<String, String> initialValue;
+
         public FileKV(String path, Map<String, String> initialValue) {
                 this.path = path;
-                this.initialValue = initialValue;
+                initialValue.entrySet().stream().forEach(entry -> set(entry.getKey(), entry.getValue()));
         }
-
-        // KeyValueStorage storage = new FileKV("src/test/resources/file", Map.of("key", "value"));
-        // Получение значения по ключу storage.get("key", "default"); // "value"
 
         @Override
         public void set(String key, String value) {
-
+                String content = Utils.readFile(path);
+                Map <String, String> data = Utils.unserialize(content);
+                data.put(key, value);
+                Utils.writeFile(path, Utils.serialize(data));
         }
 
         @Override
         public void unset(String key) {
-
+                String content = Utils.readFile(path);
+                Map <String, String> data = Utils.unserialize(content);
+                data.remove(key);
+                Utils.writeFile(path, Utils.serialize(data));
         }
 
         @Override
         public String get(String key, String defaultValue) {
-                return null;
+                Map <String, String> data = Utils.unserialize(Utils.readFile(path));
+                return data.getOrDefault(key, defaultValue);
         }
 
         @Override
         public Map<String, String> toMap() {
-                return null;
+                return new HashMap<>(Utils.unserialize(Utils.readFile(path)));
         }
-
-/*        class Utils {
-                public static String serialize(Map<String, String> map) {
-                        ObjectMapper mapper = new ObjectMapper();
-                        String json = "";
-                        try {
-                                json = mapper.writeValueAsString(map);
-                        } catch (Exception e) {
-                                System.out.println(e.getMessage());
-                        }
-                        return json;
-                }
-
-                public static Map<String, String> unserialize(String json) {
-                        ObjectMapper mapper = new ObjectMapper();
-                        Map<String, String> data = new HashMap<>();
-                        try {
-                                data = mapper.readValue(json, Map.class);
-                        } catch (Exception e) {
-                                System.out.println(e.getMessage());
-                        }
-                        return data;
-                }
-
-                public static String readFile(String path) {
-                        Path fullPath = Paths.get(path).toAbsolutePath().normalize();
-                        String content = "";
-                        try {
-                                content = Files.readString(fullPath);
-                        } catch (Exception e) {
-                                System.out.println(e.getMessage());
-                        }
-                        return content;
-                }
-
-                public static void writeFile(String path, String content) {
-                        Path fullPath = Paths.get(path).toAbsolutePath().normalize();
-                        try {
-                                Files.writeString(fullPath, content, StandardOpenOption.WRITE);
-                        } catch (Exception e) {
-                                System.out.println(e.getMessage());
-                        }
-                }
-        }*/
-
-
-
 
 }
 // END
--- java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
+++ java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
@@ -6,7 +6,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.Map;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 // BEGIN
 
 // END
@@ -23,6 +28,23 @@ class FileKVTest {
         Files.writeString(filepath, content, StandardOpenOption.CREATE);
     }
 
+    @Test
+    void fileKVTest() {
+        FileKV storage = new FileKV("src/test/resources/file", Map.of("key", "10"));
+        assertThat(storage.get("key2", "default")).isEqualTo("default");
+        assertThat(storage.get("key", "default")).isEqualTo("10");
+
+        storage.set("key2", "value2");
+        storage.set("key", "value");
+
+        assertThat(storage.get("key2", "default")).isEqualTo("value2");
+        assertThat(storage.get("key", "default")).isEqualTo("value");
+
+        storage.unset("key");
+        assertThat(storage.get("key", "def")).isEqualTo("def");
+        assertThat(storage.toMap()).isEqualTo(Map.of("key2", "value2"));
+
+    }
     // BEGIN
     
     // END
