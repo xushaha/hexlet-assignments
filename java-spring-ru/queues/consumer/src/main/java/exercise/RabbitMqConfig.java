d15f46a01cf48e8ac9d3b8de94eae0507d9761e4
@hexlet/cli <support@hexlet.io>
2023-04-10 17:10:34 +0300

    submit java-spring-ru/queues

--- java-spring-ru/queues/consumer/src/main/java/exercise/RabbitMqConfig.java
+++ java-spring-ru/queues/consumer/src/main/java/exercise/RabbitMqConfig.java
@@ -1,6 +1,9 @@
 package exercise;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +13,18 @@ public class RabbitMqConfig {
     // BEGIN
     @Bean
     Queue queue() {
+        // Задаём имя очереди
         return new Queue("queue", false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("key");
     }
     // END
 }
